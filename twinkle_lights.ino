14a66bf5bae64211f396708cd2ba731987436293
John M. Wargo <john@johnwargo.com>
2016-11-28 13:39:31 -0500

    tested code

--- twinkle_lights.ino
+++ twinkle_lights.ino
@@ -13,7 +13,7 @@ int pin0 = 9;
 int pin1 = 10;
 
 //Specifies how long the application delays between changes to output voltage
-int delayVal = 50;  //in milliseconds
+int delayVal = 20;  //in milliseconds
 
 //Constant representing max analog output
 int maxAnalog = 255;
@@ -22,7 +22,7 @@ void setup() {
   //The setup function initializes the application
   //In this case, we'll just start by winding strand 1 up to
   //full illumination
-  for (int i = 0; i < maxAnalog; i++) {
+  for (int i = 0; i <= maxAnalog; i++) {
     //Write the voltage value
     analogWrite(pin1, i);
     //Pause for a little while
@@ -34,15 +34,17 @@ void setup() {
 
 void loop() {
   //Strand 0 up, strand 1 down
-  for (int i = 0; i < maxAnalog; i++) {
+  for (int i = 0; i <= maxAnalog; i++) {
     //Write the voltage values
     analogWrite(pin0, i);
     analogWrite(pin1, maxAnalog - i);
     //Pause for a little while
     delay(delayVal);
   }
+  //Wait a second before continuing 
+  delay(1000);
   //Strand 1 up, strand 0 down
-  for (int i = 0; i < maxAnalog; i++) {
+  for (int i = 0; i <= maxAnalog; i++) {
     //Write the voltage values
     analogWrite(pin0, maxAnalog - i);
     analogWrite(pin1, i);
